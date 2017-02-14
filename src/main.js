cd601cf2e3391763f28b989d61e8de7eb50b36b4
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-02-14 19:24:50 +0530

    add : compnent support added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log("hello bro")
 
-
-module.exports = App;
+React.render(<Home />, document.getElementById('app'));
