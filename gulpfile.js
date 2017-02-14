66e5123e5a0f568b1945503e42079d412e7c9bdb
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-02-14 17:00:23 +0530

    add : js & css support added in gulp :hammer:

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,23 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local dev server
 var open = require('gulp-open'); // open a url in web browser
+var browserify = require('browserify'); // Bundle js
+var reactify = require('reactify'); // transforms react jsx to js
+var source = require('vinyl-source-stream'); // use conventional text streams with Gulp
+var concat = require('gulp-concat'); // concatenates files
 
 var config = {
     port:9000,
     devBaseUrl:'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js:'./src/**/*.js',
+        css:[
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.css'
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -37,10 +47,29 @@ gulp.task('html', function () {
        .pipe(connect.reload());
 });
 
+// path to js file and where they will land after build
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+// path to css file and where they will land after build
+gulp.task('css', function () {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
 // watch any changes made in files
 gulp.task('watch', function(){
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
 // deafult task to be run when we type gulp in command line
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
